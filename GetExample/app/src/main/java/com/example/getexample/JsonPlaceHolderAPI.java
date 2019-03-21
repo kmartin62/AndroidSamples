aa7d276769ca85654cec9e93384798afe759438d
Martin Kostadinov <martin.kostadinov@hotmail.com>
2019-03-21 13:23:53 +0100

    Example added

---
+++ GetExample/app/src/main/java/com/example/getexample/JsonPlaceHolderAPI.java
@@ -0,0 +1,12 @@
+package com.example.getexample;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderAPI {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
